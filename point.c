2f1ca6955b8c042ee91a2c80e6867e2c5ff49309
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-04-13 16:46:50 +0530

    Refactored code
    Changed the way of updating the closest_food_location in foor loop of get_closest_point

--- point.c
+++ point.c
@@ -17,8 +17,7 @@ void get_closest_point(Point food_points[], int points_length, Point current_loc
         if (distance_to_other_food < distance_to_current_food)
         {
             distance_to_current_food = distance_to_other_food;
-            closest_food_location->x = food_points[i].x;
-            closest_food_location->y = food_points[i].y;
+            *closest_food_location = food_points[i];
         }
     }
 }
